3e74940581024d56ccb3dffbb0d29dace4d29366
seemaRai902 <109678174+seemaRai902@users.noreply.github.com>
2024-09-30 10:52:22 +0530

    Create Problem set-1 solution.cpp

---
+++ Classes /Problem set-1 solution.cpp
@@ -0,0 +1,16 @@
+
+// Problems:
+
+// P1. Define a Person class like we did in the previous module. Only add name and age.
+
+// P2. Create two instances of Person and call print_info() on them.
+
+// P3. Make the attributes private and see if you can still access them from outside the class.
+
+// P4. Add a class called Student that inherits from Person. Add a field called department. Add a constructor that takes a name, age and department. Add a method called print_info() that prints out the name, age and department of the student.
+
+// P5. Create an instance of Student and call print_info() on it.
+
+// P6. Create a class called Teacher that inherits from Person. Add a field called salary. Add a constructor that takes a name, age and salary. Add a method called print_info() that prints out the name, age and salary of the teacher.
+
+// P7. Create an instance of Teacher and call print_info() on it.
