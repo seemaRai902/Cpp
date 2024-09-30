6e9abee1b2392a835409c7261c774a13c52a77fa
seemaRai902 <109678174+seemaRai902@users.noreply.github.com>
2024-09-30 10:47:59 +0530

    Update Animal_collection solutions.cpp

--- Classes /Problem set-1 solutions.cpp
+++ Classes /Problem set-1 solutions.cpp
@@ -55,22 +55,6 @@ animal->speak();
     return 0;
 }
 
-// Problems:
-
-// P1. Define a Person class like we did in the previous module. Only add name and age.
-
-// P2. Create two instances of Person and call print_info() on them.
-
-// P3. Make the attributes private and see if you can still access them from outside the class.
-
-// P7. Add a class called Student that inherits from Person. Add a field called department. Add a constructor that takes a name, age and department. Add a method called print_info() that prints out the name, age and department of the student.
-
-// P8. Create an instance of Student and call print_info() on it.
-
-// P9. Create a class called Teacher that inherits from Person. Add a field called salary. Add a constructor that takes a name, age and salary. Add a method called print_info() that prints out the name, age and salary of the teacher.
-
-// P10. Create an instance of Teacher and call print_info() on it.
-
 // Define a class AnimalCollection
 
 // Pure virtual class
