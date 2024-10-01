d5642cc3818ccb7447446656a865bfcb428d7ed7
seemaRai902 <109678174+seemaRai902@users.noreply.github.com>
2024-10-01 16:29:46 +0530

    Update Animal_collection solution.cpp

--- Classes /Animal_collection solution.cpp
+++ Classes /Animal_collection solution.cpp
@@ -1,121 +1,209 @@
-#include <string>
 #include <iostream>
-
-
+#include <cstring>
 using namespace std;
 
-// Create a class called Animal with name attribute
+// Animal base class
 class Animal {
-    // Create a constructor that takes a string as a parameter
-    // and sets the name attribute to the value of the parameter
     public:
-        Animal(string name) {
-            this->name = name;
+        virtual void speak() = 0;
+        virtual const char* getName() = 0;
+        virtual void print_info() = 0;
+        virtual ~Animal() {}  // Virtual destructor for proper cleanup
+};
+
+// Derived Dog class
+class Dog : public Animal {
+    private:
+        const char* name;
+    public:
+        Dog(const char* n) : name(n) {}
+        void speak()  {
+            cout << name << " says Woof!" << endl;
         }
-        string getName() {
+        const char* getName()  {
             return name;
         }
-        void speak() {
-            cout << "I am an animal" << endl;
+        void print_info()  {
+            cout << "Dog: " << name << endl;
         }
-        void print_info() {
-            cout << "I am an animal with name " << name << endl;
-        }
+};
+
+// Derived Person class
+class Person : public Animal {
     private:
-        string name;
-};
-
-class Dog : public Animal {
+        const char* name;
+        int age;
     public:
-        Dog(string name) : Animal(name) {
+        Person(const char* n, int a) : name(n), age(a) {}
+        void speak()  {
+            cout << name << " says Hello!" << endl;
         }
-        void speak() {
-            cout << "Woof!" << endl;
+        const char* getName()  {
+            return name;
         }
-        void bark() {
-            cout << "Bark!" << endl;
+        void print_info()  {
+            cout << "Person: " << name << ", Age: " << age << endl;
         }
 };
 
-int main() {
-    // Create an instance of the Animal class with the name "Pumba"
-    Animal pumba("Pumba");
-    // Print out the name of the animal
-    cout << pumba.getName() << endl;
-
-    Dog dog("Fido");
-    dog.speak();
-    dog.bark();
-
-    Animal *animal = new Dog("Jumbo");
-animal->speak();
-    animal->bark(); // This will not work
-
-    free(animal);
-    return 0;
-}
-
-// Define a class AnimalCollection
-
-// Pure virtual class
+// Abstract AnimalCollection class
 class AnimalCollection {
     public:
-        // Pure virtual function
-        void append(Animal animal);
-        void speak();
-        int remove(char* name);
-        int removeAt(int index);
-        int length();
-        void print_info();
+        virtual void append(Animal* animal) = 0;
+        virtual void speak() = 0;
+        virtual int remove(const char* name) = 0;
+        virtual int removeAt(int index) = 0;
+        virtual int length() = 0;
+        virtual void print_info() = 0;
+        virtual ~AnimalCollection() {}
 };
 
-// Concrete class
+// Array-based implementation
 class AnimalCollectionArray : public AnimalCollection {
     public:
-        void append(Animal animal) {
+        AnimalCollectionArray() : size(0) {}
+
+        void append(Animal* animal)  {
+            if (size < 10) animals[size++] = animal;
         }
-        void speak() {
+
+        void speak()  {
+            for (int i = 0; i < size; ++i)
+                animals[i]->speak();
         }
-        int remove(char* name) {
+
+        int remove(const char* name)  {
+            for (int i = 0; i < size; ++i) {
+                if (strcmp(animals[i]->getName(), name) == 0) {
+                    for (int j = i; j < size - 1; ++j)
+                        animals[j] = animals[j + 1];
+                    size--;
+                    return 1;
+                }
+            }
+            return 0;
         }
-        int removeAt(int index) {
+
+        int removeAt(int index)  {
+            if (index < 0 || index >= size) return 0;
+            for (int i = index; i < size - 1; ++i)
+                animals[i] = animals[i + 1];
+            size--;
+            return 1;
         }
-        int length() {
+
+        int length()  {
+            return size;
         }
-        void print_info() {
+
+        void print_info()  {
+            for (int i = 0; i < size; ++i)
+                animals[i]->print_info();
         }
-        AnimalCollectionArray() {
-        }
+
     private:
+        Animal* animals[10];  // Array of Animal pointers
+        int size;
+};
+
+// Linked-list-based implementation
+struct Node {
+    Animal* animal;
+    Node* next;
+    Node(Animal* a) : animal(a), next(nullptr) {}
 };
 
 class AnimalCollectionLL : public AnimalCollection {
     public:
-        void append(Animal animal) {
+        AnimalCollectionLL() : head(nullptr), size(0) {}
+
+        void append(Animal* animal)  {
+            Node* newNode = new Node(animal);
+            if (!head) head = newNode;
+            else {
+                Node* temp = head;
+                while (temp->next) temp = temp->next;
+                temp->next = newNode;
+            }
+            size++;
         }
-        void speak() {
-       }
-        int remove(char* name) {
+
+        void speak()  {
+            Node* temp = head;
+            while (temp) {
+                temp->animal->speak();
+                temp = temp->next;
+            }
         }
-        int removeAt(int index) {
+
+        int remove(const char* name)  {
+            Node* temp = head;
+            Node* prev = nullptr;
+            while (temp) {
+                if (strcmp(temp->animal->getName(), name) == 0) {
+                    if (prev) prev->next = temp->next;
+                    else head = temp->next;
+                    delete temp;
+                    size--;
+                    return 1;
+                }
+                prev = temp;
+                temp = temp->next;
+            }
+            return 0;
         }
-        int length() {
+
+        int removeAt(int index)  {
+            if (index < 0 || index >= size) return 0;
+            Node* temp = head;
+            Node* prev = nullptr;
+            for (int i = 0; i < index; ++i) {
+                prev = temp;
+                temp = temp->next;
+            }
+            if (prev) prev->next = temp->next;
+            else head = temp->next;
+            delete temp;
+            size--;
+            return 1;
         }
-        void print_info() {
+
+        int length()  {
+            return size;
         }
-        AnimalCollectionLL() {
+
+        void print_info()  {
+            Node* temp = head;
+            while (temp) {
+                temp->animal->print_info();
+                temp = temp->next;
+            }
         }
+
     private:
+        Node* head;
+        int size;
 };
 
-main() {
-    AnimalCollection *animals = new AnimalCollectionArray();
-    // AnimalCollection *animals = new AnimalCollectionLL();
+int main() {
+//     AnimalCollection *animals = new AnimalCollectionArray();
+     AnimalCollection *animals = new AnimalCollectionLL();
 
-    animals->append(Dog("Pumba"));
-    animals->append(Person("Alok", 50));
+    animals->append(new Dog("Pumba"));
+    animals->append(new Person("Alok", 50));
     animals->speak();
     animals->remove("Pumba");
     animals->removeAt(0);
     cout << animals->length() << endl;
+
+    delete animals;
+    return 0;
 }
+------------------------------------------------------------------------------------------------
+OUTPUT :
+Pumba says Woof!
+Alok says Hello!
+0
+----------------------------------------------------------------------------------------------------
+
+    
